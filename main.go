e204d968380557ee3867daaadb9f5b3bb230c298
viet anh <skynet0590@gmail.com>
2016-04-05 14:49:16 +0700

    add template

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
-	_ "shop/routers"
+	_ "awesome/routers"
+	_ "awesome/viewfunc"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql"
 )
 
+func init() {
+    orm.RegisterDriver("mysql", orm.DR_MySQL)
+
+    orm.RegisterDataBase("default", "mysql", "root:@/bee?charset=utf8", 30)
+}
+
 func main() {
+	
 	beego.Run()
 }
 
